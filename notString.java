ba6e582732cc18a4e9c58ad1bae97c84da628f8d
AlizeDeMatas <32222634+AlizeDeMatas@users.noreply.github.com>
2020-06-27 23:59:07 -0400

    Update notString.java

--- notString.java
+++ notString.java
@@ -6,3 +6,11 @@
 //notString("candy") → "not candy"
 //notString("x") → "not x"
 //notString("not bad") → "not bad"
+
+public String notString(String str) {
+  if(str.length() >= 3 && str.substring(0, 3).equals("not")){
+     return str; 
+  }
+  else return "not " + str; 
+}
+
